7b1cbba73f9dfc4e8bedea3d0bbe2385a5cc8248
oddlyspaced <oddlyspaced1@gmail.com>
2020-12-24 03:31:26 +0530

    avicii: Setup evolution config overlays

--- device.mk
+++ device.mk
@@ -26,7 +26,7 @@ $(call inherit-product, vendor/oneplus/avicii/avicii-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pixys
+    $(LOCAL_PATH)/overlay-evox
 
 PRODUCT_PACKAGES += \
     OnePlusIconShapeCircleOverlay \
